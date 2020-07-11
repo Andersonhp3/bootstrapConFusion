cd2e74456b530e9846c606bacb5329fd12c7e7eb
Andersonhp3 <arhp1591@gmail.com>
2020-07-10 21:55:25 -0300

    Npm scripts 2

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function () {
+    $('#mycarousel').carousel({
+        interval: 2000
+    });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+            $("#mycarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+
+    });
+    $('#login').click(function () {
+        $("#loginModal").modal('show');
+    });
+    $("#closeLogin").click(function () {
+        $("#loginModal").modal("hide")
+    });
+    $('#reserve').click(function () {
+        $("#reserveModal").modal('show');
+    });
+    $("#closeReserve").click(function () {
+        $("#reserveModal").modal("hide")
+    });
+});
